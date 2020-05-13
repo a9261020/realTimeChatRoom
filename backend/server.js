c8da82f7cd8c2c107e4b1b650d937cf07563d7ba
James <a9261020@gmail.com>
2020-05-13 17:58:59 +0800

    deploy

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,18 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 require("./socket.js")(io);
 
+// handle production
+if (process.env.NODE_ENV === "production") {
+  // static folder
+  app.use(express.static(__dirname + "/../dist/"));
+
+  // handle SPA
+  app.use(/.*/, (req, res) => res.sendFile(__dirname + "/../dist/index.html"));
+}
+
 const port = process.env.PORT || 5000;
 
-server.listen(port, err => {
+server.listen(port, (err) => {
   if (err) console.log(err);
   console.log(`server is running on ${port}`);
 });
